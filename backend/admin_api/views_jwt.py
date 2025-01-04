6749a1fa9fd586d880320e8f7bcd4240f8f5c238
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2025-01-04 19:11:54 +0300

    start from scratch

--- backend/admin_api/views_jwt.py
+++
@@ -1,5 +0,0 @@
-from .serializers import MyTokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-class CustomObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
